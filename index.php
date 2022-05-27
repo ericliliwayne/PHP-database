8614b40b64cafc5f885ab468f5928bf109b9d23d
tung wei-i <ericliliwayne@gmail.com>
2022-05-27 15:30:46 +0800

    輸出報表並做CSS美化

--- index.php
+++ index.php
@@ -9,6 +9,26 @@
         h1,h2,h3,h4{
             text-align:center;
         }
+        table{
+            border-collapse: collapse;
+            border: 3px solid blue;
+            padding: 20px;
+            margin: auto;
+        }
+        table td{
+            padding:0.5rem;
+            border: 1px solid #aaa;
+            /* text-align: center; */
+        }
+        table tr:nth-child(odd){
+            background-color: lightgreen;
+        }
+        table tr:nth-child(even){
+            background-color: lightcyan;
+        }
+        table tr:hover{
+            background-color: lightcoral;
+        }
     </style>
 </head>
 <body>
@@ -19,10 +39,22 @@
         $pdo = new PDO($dsn,'root','');
 
         $sql = "SELECT `students`.*,`dept`.`code`,`dept`.`name` as '科系' FROM `students`,`dept` WHERE `dept`.`id`=`students`.`dept`";
-        $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_BOTH);
-        echo "<pre>";
-        print_r($rows);
-        echo "</pre>";
+        $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        echo "<table>";
+        foreach($rows as $row){
+            echo "<tr>";
+                echo"<td>{$row['id']}</td>";
+                echo"<td>{$row['school_num']}</td>";
+                echo"<td>{$row['name']}</td>";
+                echo"<td>{$row['科系']}</td>";
+                echo"<td>{$row['parents']}</td>";
+                echo"<td>{$row['addr']}</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // echo "<pre>";
+        // print_r($rows);
+        // echo "</pre>";
         // echo $rows[0][0];
         // echo "<br>";
         // echo $rows[0]['id'];
